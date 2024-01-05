796bf178fb62e885b82bf20d94c00408b661c212
njhaus <njhaus@gmail.com>
2024-01-05 15:42:25 -0600

    Update About me

--- src/components/pages/AboutMe.tsx
+++ src/components/pages/AboutMe.tsx
@@ -7,29 +7,31 @@ const AboutMe = () => {
       <p>
         <span className="bold">Welcome! I'm Nick Haus, Web Developer.</span> I
         am a full-stack web developer with a passion for creating beautiful,
-        dynamic, and accessible web applications. I am eager to leverage my technical expertise and collaborative
-        skills to contribute to a forward-thinking company at the forefront of
-        innovation.
+        dynamic, and accessible web applications. Along with my technical
+        skills, I am also a proven team player and lifelong learner with
+        excellent customer service, teamwork, and organization skills. I am
+        looking to join a modern, innovative company with a forward-thinking,
+        outward focus.
       </p>
       <p>
         My web development journey encompasses comprehensive training in front
         and back-end technologies, including, but not limited to, MERN Stack
-        technology, SQL, Python, RESTful APIs, and front-end languages such as
-        TypeScript, JavaScript, HTML, and CSS. I have a high aptitude for
-        self-starting and self-directed learning, and have become adept at using
-        available resources such as technical documentation to achieve my goals.
+        technology, SQL, Python, RESTful APIs and CRUD operations, Typescript,
+        Javascript, HTML, and CSS. I excel in self-directed learning, and have become adept at using available
+        resources such as technical documentation to achieve my goals.
         Currently, I am participating in Chingu Cohorts, a program in which
         teams of developers gain real-world experinece by developing
         applications using Agile methodology.
       </p>
       <p>
         Having spent 12 years as a professional in the education field, I have
-        honed invaluable skills that transcend industries. As an educator, I
-        worked at the forefront of educational technology, piloting educational
-        platforms and devices and initiating programs that encouraged students
-        to explore the world of tech. In transitioning to Web development, I
-        hope to continue to work with professional teams, produce inspiring and
-        technically-sound creations, and influence the world.
+        put into practice the personal skills and compentecies necessary to
+        thrive in a fast-paced, ever-changing professional environment. As an
+        educator, I worked at the forefront of educational technology, piloting
+        educational platforms and devices and initiating programs that
+        encouraged students to explore the world of tech. In transitioning to
+        web development, I hope to continue to work with professional teams, produce
+        inspiring and technically-sound creations, and influence the world.
       </p>
     </div>
   );
