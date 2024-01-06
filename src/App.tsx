23dcbec613c8daf22390eb6ee220127c7de27ed3
njhaus <njhaus@gmail.com>
2024-01-05 18:45:33 -0600

    Reorganize style files

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,23 @@ import AboutMe from './components/pages/AboutMe';
 import Resume from './components/pages/Resume';
 import Certificates from './components/pages/Certificates';
 
+// Styles
+// Root and body
 import './App.css'
 import './styles/utility_styles.css'
 import './styles/animations.css'
+// Header, contact me dialog, footer
+import './styles/header.css'
+// main section, pages container
+import './styles/pages/pages.css'
+// Individual pages
+import "./styles/pages/welcome.css";
+import "./styles/pages/portfolio-certificate.css";
+import "./styles/pages/resume-about.css";
+// Media Queries
+import "./styles/media-queries.css";
+
+
 import { useState } from 'react';
 import Contact from './components/parts/Contact';
 
