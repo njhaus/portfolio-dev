a272f4d55a72e135db1c16524fd937696f9891f2
njhaus <126741288+njhaus@users.noreply.github.com>
2024-02-05 16:07:25 -0600

    Remove unused variables

--- src/pages/sections/portfolio/Portfolio.tsx
+++ src/pages/sections/portfolio/Portfolio.tsx
@@ -4,32 +4,7 @@ import { portfolioData } from "../../../data/portfolioData";
 import Project from "../portfolio/components/Project";
 
 const Portfolio = () => {
-  const [scrollTo, setScrollTo] = useState(0);
-
-  const handleScroll = () => {
-    const projectContainer = document.getElementById("portfolio-container");
-    const projects = projectContainer?.children
-      ? Array.from(projectContainer?.children)
-      : [];
-    console.log(projects[scrollTo + 1]);
-    if (scrollTo + 1 < projects.length) {
-      console.log("scrolling");
-      projects[scrollTo + 1].scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-        inline: "end",
-      });
-      setScrollTo(scrollTo + 1);
-    } else {
-      projects[0].scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "end",
-      });
-      setScrollTo(0);
-    }
-  };
-
+  
   return (
     <div className="w-full px-8 py-6 h-fit mb-24">
       <h1 className="section-heading">My Projects</h1>
@@ -46,9 +21,6 @@ const Portfolio = () => {
           ></Project>
         ))}
       </div>
-      {/* <p className="scroll" id="scroll" onClick={() => handleScroll()}>
-        scroll &#9660;
-      </p> */}
     </div>
   );
 };
